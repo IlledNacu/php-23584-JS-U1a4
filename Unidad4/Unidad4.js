99dbc8c0f77eeb52bf09bdde68cdcbfa1ca2d2fb
IlledNacu <bkilled7@gmail.com>
2023-10-26 13:36:59 -0300

    unidad 4

--- Unidad4/Unidad4.js
+++ Unidad4/Unidad4.js
@@ -4,6 +4,20 @@
 // 22. Se le solicita al usuario que ingrese 3 números. Realice un programa para informar 
 // si el número es múltiplo de 3, múltiplo 5, múltiplo de ambos o múltiplo de ninguno.
 
+var a = parseInt(prompt('Ingrese un número'))
+var b = parseInt(prompt('Ingrese otro número'))
+var c = parseInt(prompt('Ingrese un número más'))
+
+if((a+b+c)/3 == 0 & (a+b+c)/5 == 0){
+    alert('Es múltiplo de ambos')
+}
+else if((a+b+c)/3 == 0){
+    alert('Es múltiplo de 3')}
+else if((a+b+c)/5 == 0){
+    alert('Es múltiplo de 5')
+}
+else{alert('No es múltiplo de ninguno')}
+
 // 23. Se le solicita al usuario que ingrese los extremos de un rango numérico y una 
 // cantidad de valores desconocidos.
 // Realice un programa para informar si cada número está dentro del rango.
@@ -12,10 +26,68 @@
 // Tenga en cuenta que el rango debe tener una diferencia mínima de 5 números 
 // enteros.
 
+var inicio = parseInt(prompt('Ingrese el origen del rango'))
+var fin = parseInt(prompt('Ingrese el fin del rango'))
+if((fin-inicio)<5){
+    fin = parseInt(prompt('El rango debe tener una diferencia mínima de 5. Ingrese otro fin'))
+}
+
+var numeros = []
+var opcion = true
+
+while(opcion){
+    var numero = prompt('Ingrese un número. Cuando no quiera ingresar más presione')
+    if(numero >= 'a' & numero <= 'z' & numero >= 'A' & numero <= 'Z'){
+        opcion = false
+    }
+    else{
+        numeros += parseInt(numero)
+    }
+}
+
+for(let num = 0;numeros.length-1;num++){
+    if(numero[num]>=inicio&numero[num]<=fin){
+        if(numero[num]%2 == 0){
+            alert('Es par')
+        }
+    }
+    else{
+        if(numero[num]%2 != 0){
+            alert('Es impar')
+        }
+    }
+}
+
 // 24. Se le solicita al usuario que ingrese dos números y un operador (+, -, *, /). Realice 
 // un programa para calcular e informar cada operación deseada entre los dos números. 
 // Ese proceso se puede repetir mientras lo desee el usuario. Todos los valores deben ser 
 // validados.
+
+opcion = true
+while(opcion){
+    a = parseInt(prompt('Ingrese un número'))
+    b = parseInt(prompt('Ingrese otro número'))
+
+    c = prompt('Ingrese el operador de la operación que desee realizar (+, -, *, /)')
+    while(c!='+'&c!='-'&c!='*'&c!='/'){
+        c = prompt('Ingrese el operador de la operación que desee realizar (+, -, *, /)')
+    }
+    if(c=='+'){
+        alert( a + b )
+    }
+    else if(c=='-'){
+        alert( a - b )
+    }
+    else if(c=='*'){
+        alert ( a * b )
+    }
+    else{ alert ( a / b ) }
+
+    continuar = prompt('Desea continuar? S/N')
+    if(continuar == 'N'|continuar == 'n'){
+        opcion = false
+    }
+}
 
 // 25. Se le solicita al usuario que ingrese las notas del parcial de los alumnos de una 
 // comisión. Se desconoce la cantidad de alumnos. Realice el algoritmo para informar:
